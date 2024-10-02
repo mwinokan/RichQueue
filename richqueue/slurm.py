f59d4b907d0211ab5a41b0af6a41f7da5da2269f
Max Winokan <mwinokan@me.com>
2024-10-02 22:33:27 +0100

    idle demo

--- richqueue/slurm.py
+++ richqueue/slurm.py
@@ -28,6 +28,16 @@ def color_by_state(state):
     else:
         return state
 
+def color_by_node_state(state):
+
+    if state == "ALLOCATED":
+        return "[orange1]Allocated"
+    elif state == "MIXED":
+        return "[bright_yellow]Mixed"
+    elif state == "IDLE":
+        return "[bright_green]Idle"
+    else:
+        return f"[red]{state}"
 
 def extract_inner(df, key, inner):
 
@@ -71,6 +81,81 @@ def extract_list(df, key):
 
     df[key] = df.apply(inner, axis=1)
 
+def mem_string(mb):
+    
+    # gb, mb = divmod(mb, 1024)
+
+    # return f"{gb}G {mb}M"
+
+    return f"{mb/1024:.0f}G"
+
+def node_table(
+    df,
+    title: str = None,
+    box: bool = False,
+    panel: bool = True,
+    idle: bool = True,
+):
+
+    if idle:
+        df = df[df["node"].isin(["IDLE", "MIXED"])]
+        title = title or "idle nodes" 
+    else:
+        title = title or "nodes"
+
+    from rich.table import Table
+
+    if box:
+        from rich.box import SIMPLE
+
+        box = SIMPLE
+
+
+
+
+    table = Table(title=title, box=box, header_style="")
+
+    # for col in df.columns:
+    #     table.add_column(col)
+
+    print(df.columns)
+
+
+    table.add_column("[bold underline]Node", justify='left', no_wrap=True, style="bold")
+    table.add_column("[bold underline]State", justify='left', no_wrap=True, style="bold")
+    table.add_column("[green_yellow underline]Partition", justify='left', no_wrap=True, style="green_yellow")
+    table.add_column("[magenta underline]#C", justify='right', no_wrap=True, style="magenta")
+    table.add_column("[magenta underline]Memory", justify='right', no_wrap=True, style="magenta")
+    table.add_column("[dodger_blue2 underline]Features", justify='left', no_wrap=True, style="dodger_blue2")
+
+    reservation = any(df["reservation"].values)
+
+    if reservation:
+        table.add_column("[bold underline]Reservation", justify='left', no_wrap=True, style="bold")
+
+    for i,row in df.iterrows():
+
+        values = []
+
+        values.append(row.nodes)
+        # values.append(row.nodes)
+        values.append(color_by_node_state(row.node))
+        values.append(row.partition)
+        values.append(str(row.cpus_idle))
+        values.append(mem_string(row.memory_free))
+        values.append(row.features)
+
+        if reservation:
+            values.append(row.reservation)
+
+        table.add_row(*values)
+
+        # table.add_row(*[str(v) for v in row.values])
+
+    if panel:
+        return Panel(table, expand=False)
+
+    return table
 
 def job_table(
     df,
@@ -366,13 +451,47 @@ def dual_layout(
     else:
         return layout
 
+def parse_sinfo_json(payload: dict) -> "DataFrame":
+
+    df = DataFrame(payload['sinfo'])
+
+    df2 = df.drop(columns=["port", "weight", "disk", "sockets", "threads", "cluster", "comment", "extra", "gres", "reason", "cores"])
+    df2["node"] = df2.apply(lambda x: x["node"]["state"][0], axis=1)
+    df2["nodes"] = df2.apply(lambda x: x["nodes"]["nodes"][0], axis=1)
+    df2["cpus_max"] = df2.apply(lambda x: x["cpus"]["maximum"], axis=1)
+    df2["cpus_idle"] = df2.apply(lambda x: x["cpus"]["idle"], axis=1)
+    df2["cpus_allocated"] = df2.apply(lambda x: x["cpus"]["allocated"], axis=1)
+    df2["memory_max"] = df2.apply(lambda x: x["memory"]["maximum"], axis=1)
+    df2["memory_free"] = df2.apply(lambda x: x["memory"]["free"]["maximum"]["number"], axis=1)
+    df2["memory_allocated"] = df2.apply(lambda x: x["memory"]["allocated"], axis=1)
+    df2["features"] = df2.apply(lambda x: x["features"]["total"], axis=1)
+    df2["partition"] = df2.apply(lambda x: x["partition"]["name"], axis=1)
+
+    df2.drop(columns=["cpus", "memory"], inplace=True)
+
+    return df2
+
+def idle_queue():
+    
+    # panel = Panel()
+
+    # table = Table()
+
+    df = parse_sinfo_json(json.load(open('example_data/sinfo_N.json')))
+
+    table = node_table(df)
+
+    console.print(table)
 
 @app.command()
-def show(user: None | str = None, long: bool = False):
+def show(user: None | str = None, long: bool = False, idle: bool = False):
 
     loop = True
 
-    if loop:
+    if idle:
+        idle_queue()
+
+    elif loop:
 
         from rich.live import Live
         import time
@@ -392,7 +511,6 @@ def show(user: None | str = None, long: bool = False):
         console.print(layout)
         # show_queue(command="squeue", user=user, long=long)
         # show_queue(command="sacct", user=user, long=long)
-
 
 def main():
     app()
