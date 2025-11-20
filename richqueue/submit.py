3a202968928fb13fb758633fd1fed1d4768f1f62
Max Winokan <mwinokan@me.com>
2025-11-20 10:55:48 +0000

    sb help info

--- richqueue/submit.py
+++ richqueue/submit.py
@@ -9,6 +9,24 @@ from .console import console
 
 def submit():
 
+    sbatch_args = sys.argv[1:]
+
+    if not sbatch_args:
+        console.print("[red bold]Please provide sbatch arguments")
+        console.print(
+            "\n[bold yellow]Usage:[reset][bold] sb [SBATCH OPTIONS] SCRIPT [SCRIPT ARGS]...\n"
+        )
+        sys.exit(1)
+
+    elif len(sbatch_args) == 1 and sbatch_args[0] in ["-h", "--help"]:
+        console.print(
+            "\n[bold]sb[reset]: a pretty wrapper to the sbatch command that places log files in the directory specified by the LOGS variable"
+        )
+        console.print(
+            "\n[bold yellow]Usage:[reset][bold] sb [SBATCH OPTIONS] SCRIPT [SCRIPT ARGS]...\n"
+        )
+        sys.exit(1)
+
     log_dir = environ.get("LOGS")
 
     if not log_dir:
@@ -16,8 +34,6 @@ def submit():
         sys.exit(1)
 
     log_dir = Path(log_dir)
-
-    sbatch_args = sys.argv[1:]
 
     commands = [
         "sbatch",
